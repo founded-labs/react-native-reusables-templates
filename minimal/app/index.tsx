3652f10eb7ddb68079bc84b7a771b023d99fa8a4
Zach Nugent <mrzachnugent@gmail.com>
2025-07-30 21:33:51 -0400

    fix(minimal): android toggle and header shadow issue

--- minimal/app/index.tsx
+++ minimal/app/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
+import { THEME } from '@/lib/theme';
 import { Link, Stack } from 'expo-router';
 import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 import * as React from 'react';
-import { Image, View } from 'react-native';
+import { Image, type ImageStyle, View } from 'react-native';
 
 const LOGO = {
   light: require('@/assets/images/react-native-reusables-light.png'),
@@ -13,10 +14,25 @@ const LOGO = {
 };
 
 const SCREEN_OPTIONS = {
-  title: 'React Native Reusables',
-  headerTransparent: true,
-  headerShadowVisible: true,
-  headerRight: () => <ThemeToggle />,
+  light: {
+    title: 'React Native Reusables',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.light.background },
+    headerRight: () => <ThemeToggle />,
+  },
+  dark: {
+    title: 'React Native Reusables',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.dark.background },
+    headerRight: () => <ThemeToggle />,
+  },
+};
+
+const IMAGE_STYLE: ImageStyle = {
+  height: 76,
+  width: 76,
 };
 
 export default function Screen() {
@@ -24,14 +40,16 @@ export default function Screen() {
 
   return (
     <>
-      <Stack.Screen options={SCREEN_OPTIONS} />
+      <Stack.Screen options={SCREEN_OPTIONS[colorScheme ?? 'light']} />
       <View className="flex-1 items-center justify-center gap-8 p-4">
-        <Image source={LOGO[colorScheme ?? 'light']} height={85} width={96} />
+        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
         <View className="gap-2 p-4">
-          <Text className="font-mono text-sm">
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
             1. Edit <Text variant="code">app/index.tsx</Text> to get started.
           </Text>
-          <Text className="font-mono text-sm">2. Save to see your changes instantly.</Text>
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            2. Save to see your changes instantly.
+          </Text>
         </View>
         <View className="flex-row gap-2">
           <Link href="https://reactnativereusables.com" asChild>
@@ -61,10 +79,10 @@ function ThemeToggle() {
 
   return (
     <Button
-      onPress={toggleColorScheme}
+      onPressIn={toggleColorScheme}
       size="icon"
       variant="ghost"
-      className="rounded-full web:mr-4">
+      className="rounded-full web:mx-4">
       <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
     </Button>
   );
